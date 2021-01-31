c97b734df94e92f2b96365eba89d0b9e5adf689c
BELGHITI Mouad <Mouad.BELGHITI@akka.eu>
2021-02-01 00:30:54 +0100

    finalisation landing page without artisan redirection

--- smartInitiatives/src/app/header/header.component.ts
+++ smartInitiatives/src/app/header/header.component.ts
@@ -1,37 +1,28 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
-import {ClipboardModule} from '@angular/cdk/clipboard';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  nbr ="06 12001200";
-  constructor() {}
+  nbr = "06 12001200";
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {}
-  scroll(id) {
-    const el: HTMLElement | null = document.getElementById(id);
-    if (el) {
-      el.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-        inline: 'nearest',
-      });
-    }
-  }
-  copyNumber(val: string){
+  ngOnInit(): void { }
+  copyNumber(val: string) {
     let selBox = document.createElement('textarea');
-      selBox.style.position = 'fixed';
-      selBox.style.left = '0';
-      selBox.style.top = '0';
-      selBox.style.opacity = '0';
-      selBox.value = val;
-      document.body.appendChild(selBox);
-      selBox.focus();
-      selBox.select();
-      document.execCommand('copy');
-      document.body.removeChild(selBox);
-    }
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
 
 }
