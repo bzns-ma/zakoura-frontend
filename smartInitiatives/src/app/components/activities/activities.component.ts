9011b3acd158503a5a4d8ba7131055671045abfa
Mouad <belghiti.mouad.1@gmail.com>
2021-05-31 11:19:18 +0200

    inject activities data directly in component

--- smartInitiatives/src/app/components/activities/activities.component.ts
+++ smartInitiatives/src/app/components/activities/activities.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivitiesService } from '../../services/activities.service';
 import { Activity } from '../../models/activity';
+import { environment } from '../../../environments/environment';
 @Component({
   selector: 'app-activities',
   templateUrl: './activities.component.html',
@@ -9,6 +10,42 @@ import { Activity } from '../../models/activity';
 export class ActivitiesComponent implements OnInit {
 
   activities: Activity[] = [];
+  activitiesData = [{
+    "title": "Accueil et Orientation",
+    "description": "Offrir un service d’accueil et d’orientation socio-professionnelle au profit de la population locale",
+    "iconclass":"lni-direction-alt"
+},
+{
+    "title": "Employabilité",
+    "description": "Renforcer les aspects de l’employabilité des jeunes chercheurs d’emplois",
+    "iconclass":"lni-briefcase"
+
+},
+{
+    "title": "Accompagnement",
+    "description": "Accompagner les porteurs de projets dans les démarches de la maturité entrepreneuriale",
+    "iconclass":"lni-customer"
+
+},
+{
+    "title": "Innovation sociale",
+    "description": "Renforcer la cohésion entre les collaborateurs du SMT et la commune à travers l’innovation sociale",
+    "iconclass":"lni-invention"
+
+},
+{
+    "title": "Inclusion sociale",
+    "description": "Renforcer les aspects d’inclusion sociale auprès des populations vulnérables.",
+    "iconclass":"lni-handshake"
+
+},
+{
+    "title": "Synergies territoriales",
+    "description": "Construire une stratégie territoriale commune pour le développement économique et social.",
+    "iconclass":"lni-users"
+
+}
+]
 
   constructor(private api: ActivitiesService) { }
 
@@ -17,10 +54,11 @@ export class ActivitiesComponent implements OnInit {
   }
 
   getActivities() {
-    this.api.getActivities().subscribe(response => {
-      for (const data of response.body) {
-        this.activities.push(data);
-      }
-    });
+    // this.activities = this.activitiesData;
+    // this.api.getActivities().subscribe(response => {
+    //   for (const data of response.body) {
+    //     this.activities.push(data);
+    //   }
+    // });
   }
 }
